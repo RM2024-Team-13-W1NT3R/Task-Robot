78aa158de5b8e2d1bb10d9a32d77ca1ac2b5c7ec
SunnyFarmDay <SunnyFarmDay@gmail.com>
2023-10-29 15:07:05 +0800

    added connection status, decoding, callback, init, rpm variables

--- RM2024-RDC-Core/Drivers/DR16.cpp
+++ RM2024-RDC-Core/Drivers/DR16.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "DR16.hpp"
+
 #if USE_DR16
 
 namespace DR16
@@ -15,15 +16,75 @@ namespace DR16
  * @remark Refer to the definition of the structure in the "DR16.hpp" files
  */
 static RcData rcData;
-
+bool rcConnected = false;
 /*Return the constant pointer of the current decoded data*/
 const RcData *getRcData() { return &rcData; }
+uint32_t lastUpdatedTime;
 
 /*================================================================================*/
 /*You are free to declare your buffer, or implement your own function(callback, decoding) here*/
+const uint8_t DR16_FRAME_LENGTH = 18;
+uint8_t rcRxBuffer[DR16::DR16_FRAME_LENGTH];
+
+void resetRcData() {
+    rcData.channel0 = 1024;
+    rcData.channel1 = 1024;
+    rcData.channel2 = 1024;
+    rcData.channel3 = 1024;
+    rcData.s1 = 3;
+    rcData.s2 = 3;
+}
+
+bool validateRcData() {
+    if ((rcData.channel0 > 1684 || rcData.channel0 < 364) ||
+        (rcData.channel1 > 1684 || rcData.channel1 < 364) ||
+        (rcData.channel2 > 1684 || rcData.channel2 < 364) ||
+        (rcData.channel3 > 1684 || rcData.channel3 < 364) ||
+        (rcData.s1 > 3 || rcData.s1 < 1) ||
+        (rcData.s2 > 3 || rcData.s2 < 1)) {
+        return false;
+    }
+    return true;
+}
+
+void decodeRcData() {
+    rcData.channel0 = ((uint16_t) rcRxBuffer[0] | (uint16_t) rcRxBuffer[1] << 8) & 0x07FF;
+    rcData.channel1 = ((uint16_t) rcRxBuffer[1] >> 3 | (uint16_t) rcRxBuffer[2] << 5) & 0x07FF;
+    rcData.channel2 = ((uint16_t) rcRxBuffer[2] >> 6 | (uint16_t) rcRxBuffer[3] << 2 | rcRxBuffer[4] << 10) & 0x07FF;
+    rcData.channel3 = ((uint16_t) rcRxBuffer[4] >> 1 | (uint16_t) rcRxBuffer[5] << 7) & 0x07FF;
+    rcData.s1 = ((uint16_t) rcRxBuffer[5] >> 4) & 0x0003;
+    rcData.s2 = ((uint16_t) rcRxBuffer[5] >> 6) & 0x0003;
+}
+
+void rxEventCallback(UART_HandleTypeDef *huart, uint16_t datasize) {
+    if (huart->Instance == USART1) {
+        decodeRcData();
+        HAL_UARTEx_ReceiveToIdle_IT(huart, rcRxBuffer, DR16::DR16_FRAME_LENGTH);
+        if (!validateRcData()) {
+            resetRcData();
+        }
+        lastUpdatedTime = HAL_GetTick();
+    }
+}
 
 
 
+bool getIsRcConnected() {
+    uint32_t currentTime = HAL_GetTick();
+    if ((lastUpdatedTime + 1000 > currentTime )) {
+        rcConnected = true;
+        return rcConnected;
+    } else {
+        rcConnected = false;
+        resetRcData();
+        return rcConnected;
+    }
+}
+
+const bool *getRcConnected() {
+    getIsRcConnected();
+    return &rcConnected;
+}
 
 
 
@@ -31,6 +92,9 @@ const RcData *getRcData() { return &rcData; }
 void init()
 {
     /*If you would like to, please implement your function definition here*/
+    resetRcData();
+    HAL_UART_RegisterRxEventCallback(&huart1, DR16::rxEventCallback);
+    HAL_UARTEx_ReceiveToIdle_IT(&huart1, rcRxBuffer, DR16::DR16_FRAME_LENGTH);
 }
 
 }  // namespace DR16
--- RM2024-RDC-Core/Drivers/DR16.hpp
+++ RM2024-RDC-Core/Drivers/DR16.hpp
@@ -10,6 +10,8 @@
  */
 #pragma once // In case of multiple inclusion
 #include "AppConfig.h"
+#include "stdint.h"
+#include "usart.h"
 #if USE_DR16
 
 
@@ -21,20 +23,42 @@ namespace DR16
 
 /**
  * @brief the DR16 remote controller structure
- * @brief This structure is should be based on the DR16 user manual
- * @note  Now you have to refer to the DR16 User Manual and define your own data structure
+ *
+ * @param channel0 The channel0 data from the remote controller
+ * @param channel1 The channel1 data from the remote controller
+ * @param channel2 The channel2 data from the remote controller
+ * @param channel3 The channel3 data from the remote controller
+ *
+ * @param s1       The switch 1 data from the remote controller
+ * @param s2       The switch 2 data from the remote controller
  */
 typedef struct
 {
-    /*Your own implementation*/
+    uint16_t channel0 : 11;  // Channel 0 data
+    uint16_t channel1 : 11;  // Channel 1 data
+    uint16_t channel2 : 11;  // Channel 2 data
+    uint16_t channel3 : 11;  // Channel 3 data
 
+    uint8_t s1 : 2;  // Switch 1 data
+    uint8_t s2 : 2;  // Switch 2 data
 } RcData;
+
+typedef struct
+{
+    float motor0;
+    float motor1;
+    float motor2;
+    float motor3;
+} MotorRPM;
+float maxRPM = 5;
+
+void limitRPM();
 
 /**
  * @brief Access the decoded remote controller datat by this API
  * @remark You are recommended to:
  *         - Return a constant pointer of the decoded remote controller data
- * inthis fucntion
+ * in this fucntion
  */
 const RcData *getRcData();
 
@@ -43,6 +67,13 @@ const RcData *getRcData();
  * more complicated DR16 module*/
 
 
+/**
+ * @brief Get the status of the remote controller
+ * 
+ * @return true if the remote controller is connected
+ * @return false if the remote controller is not connected
+ */
+const bool *getRcConnected();
 
 /*===========================================================================*/
 
